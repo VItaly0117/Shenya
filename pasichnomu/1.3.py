298a5f438e926f35ac968fcf71a77782b229fe4f
TertMaxe <terticnijmaksim47@gmail.com>
2026-02-17 19:23:52 +0200

    фіаіфа

--- pasichnomu/1.3.py
+++ pasichnomu/1.3.py
@@ -10,24 +10,19 @@ elements={  #first columnt represents A*10**-23 Дж,second - Z*10**-10 m
     "Xe": [320e-23, 3.98e-10]
 }
 
-
+R = np.linspace(2e-10, 1e-9, 500)
 def calculations(A,Z):
-    R = np.linspace(0.93*Z, 2.5*Z, 500)
+
     U1=(Z/R)**12 #сили відштовхування
     U2=(Z/R)**6 #сили притягання
     U=4*A*(U1-U2)
-    return R, U
+    return U
 
+for name, data in elements.items():
+    U = calculations(data[0], data[1])
+    plt.plot(R, U)
 
-HeR,HeU=calculations(elements["He"][0],elements["He"][1])
-plt.plot(HeR,HeU)
-NeR,NeU=calculations(elements["Ne"][0],elements["Ne"][1])
-plt.plot(NeR,NeU)
-ArR,ArU=calculations(elements["Ar"][0],elements["Ar"][1])
-plt.plot(ArR,ArU)
-KrR,KrU=calculations(elements["Kr"][0],elements["Kr"][1])
-plt.plot(KrR,KrU)
-XeR,XeU=calculations(elements["Xe"][0],elements["Xe"][1])
-plt.plot(XeR,XeU)
+plt.ylim(-0.5e-20, 0.5e-20)
+plt.xlim(2e-10,6e-10)
 plt.grid(True)
 plt.show()
